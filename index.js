bff9e4d3b301ee4ad63e86bc19191eb0330b1e5d
lesagecl <lesagecl@dukes.jmu.edu>
2021-10-25 17:03:17 -0400

    mysql and credentials

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const express = require('express');
+// const fetch = require('node-fetch');
 
 const service = express();
 
+
 const array = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512];
 
+const port = 5000;
+service.listen(port, () => {
+    console.log(`We're live on port ${port}!`);
+});
+
+// sql credentials & parsing
+const fs = require('fs');
+const mysql = require('mysql');
+
+const json = fs.readFileSync('credentials.json', 'utf8');
+const credentials = JSON.parse(json);
+
+const connection = mysql.createConnection(credentials);
+connection.connect(error => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
+
+// TODO: issue queries
+const selectQuery = 'SELECT * FROM schedule';
+connection.query(selectQuery, (error, rows) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(rows);
+  }
+});
+
+connection.end();
+
+// permissions
 service.options('*', (request, response) => {
     response.set('Access-Control-Allow-Headers', 'Content-Type');
     response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
     response.sendStatus(200);
   });
 
-  // define endpoints:
-
+// TODO: define endpoints
 service.get('/all', (request, response) => {
     response.json({
         ok: true,
@@ -25,20 +59,15 @@ service.get('/one/:index', (request, response) => {
       ok: true,
       result: array[index],
     });
-  });
+});
 
-  // client i think?:
-
-  fetch('http://localhost:5000/all')
-  .then(assertResponse)
-  .then(response => response.json())
-  .then(data => {
-    if (data.ok) {
-      console.log(data);
-    }
-  }).
-  catch(error => console.error(error));
-const port = 5000;
-service.listen(port, () => {
-    console.log(`We're live on port ${port}!`);
-  });
+// client i think?
+// fetch('http://localhost:5000/all')
+// .then(assertResponse)
+// .then(response => response.json())
+// .then(data => {
+//     if (data.ok) {
+//       console.log(data);
+//     }
+// }).
+// catch(error => console.error(error));
