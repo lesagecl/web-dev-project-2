335adab0ce751eb8b4dba9d627d0ee791b40496e
lesagecl <lesagecl@dukes.jmu.edu>
2021-10-26 12:12:20 -0400

    port 5001

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const service = express();
 
-const port = 5000;
+const port = 5001;
 service.listen(port, () => {
     console.log(`We're live on port ${port}!`);
 });
