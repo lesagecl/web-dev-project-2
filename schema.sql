c3ea3f62eabe303f63988b8a52989e02403c163e
lesagecl <lesagecl@dukes.jmu.edu>
2021-12-07 10:17:15 -0500

    times are now ints

--- schema.sql
+++ schema.sql
@@ -4,11 +4,11 @@ CREATE TABLE schedule (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(10),
     start_time VARCHAR(10),
-    end_time VARCHAR(10),
-    week_day VARCHAR(10),
+    end_time INT,
+    week_day INT,
     is_deleted INT DEFAULT 0
 );
 
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', '14:00', '17:00', 'Wednesday', 0);
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', '14:00', '17:00', 'Monday', 0);
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', '10:00', '13:00', 'Friday', 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 2, 4, 'Wednesday', 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 7, 11, 'Monday', 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 1, 4, 'Friday', 0);
