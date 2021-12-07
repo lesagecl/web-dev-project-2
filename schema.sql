4cc9ca09d32560e2b68c145a0488249dd0d9169f
lesagecl <lesagecl@dukes.jmu.edu>
2021-12-07 13:40:25 -0500

    weekdays now ints

--- schema.sql
+++ schema.sql
@@ -5,12 +5,12 @@ CREATE TABLE schedule (
     first_name VARCHAR(10),
     start_time INT,
     end_time INT,
-    week_day VARCHAR(10),
+    week_day INT,
     is_deleted INT DEFAULT 0
 );
 
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 2, 4, 'Wednesday', 0);
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 7, 11, 'Monday', 0);
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 1, 4, 'Friday', 0);
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('Jane', 0, 3, 'Thursday', 0);
-INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('Jane', 6, 12, 'Thursday', 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 2, 4, 3, 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 7, 11, 1, 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('John', 1, 4, 5, 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('Jane', 0, 3, 4, 0);
+INSERT INTO schedule (first_name, start_time, end_time, week_day, is_deleted) VALUES ('Jane', 6, 12, 4, 0);
